fd47277f5f6d774a6080c81a88c1540a9fe1ae87
unknown <miroslav.nedelchev@dentacoin.com>
2018-10-25 13:30:07 +0300

    updates

--- public/assets/js/index-compiled.js
+++ public/assets/js/index-compiled.js
@@ -26692,6 +26692,7 @@ function styleInputTypeFile(load_filename_to_other_el) {
 
                 reader.addEventListener('load', function (e) {
                     if (isJsonString(e.target.result) && has(JSON.parse(e.target.result), 'address')) {
+                        var address = JSON.parse(e.target.result).address;
                         //init upload button animation
                         initCustomInputFileAnimation(label);
 
@@ -26706,7 +26707,8 @@ function styleInputTypeFile(load_filename_to_other_el) {
                                     type: 'POST',
                                     url: HOME_URL + '/app-import',
                                     data: {
-                                        password: this_btn.attr('data-password')
+                                        password: this_btn.attr('data-password'),
+                                        address: address
                                     },
                                     dataType: 'json',
                                     success: function success(response) {
