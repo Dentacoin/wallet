7ff12e65624577cb449e60f3b13b0581e1356175
unknown <miroslav.nedelchev@dentacoin.com>
2018-10-25 15:28:44 +0300

    updates

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -127,10 +127,10 @@
             </div>
         </div>
     </footer>
-    <script src="/dist/js/front-libs-script.js?v=1.0.28"></script>
+    <script src="/dist/js/front-libs-script.js?v=1.0.29"></script>
     <script src="/assets/js/basic.js?v=1.0.17"></script>
     @yield("script_block")
     {{--<script src="/dist/js/front-script.js"></script>--}}
-    <script src="/assets/js/index-compiled.js?v=1.1.70"></script>
+    <script src="/assets/js/index-compiled.js?v=1.1.71"></script>
 </body>
 </html>
