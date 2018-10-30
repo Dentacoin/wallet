a027e31531310d3a5eb8a9d318d97cc2f52f01ad
unknown <miroslav.nedelchev@dentacoin.com>
2018-10-30 09:29:48 +0200

    testing trasanctions

---
+++ resources/views/pages/maintenance.blade.php
@@ -0,0 +1,4 @@
+<div style="text-align: center; padding: 200px 20px 200px;font-family: Calibri, sans-serif;">
+    <h2 style="font-weight: bold;font-size: 28px;">Temporally down for Maintenance</h2>
+    <p>We are performing scheduled maintenance. We should be back online shortly.</p>
+</div>
