ddc7cbf381b0c3b9b5bccbc9eb8c80616a68737e
unknown <miroslav.nedelchev@dentacoin.com>
2019-02-15 17:15:08 +0200

    test pub key creation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -161,10 +161,8 @@ class Controller extends BaseController {
     }
 
     protected function savePublicKey(Request $request) {
-        var_dump('asd');
-        die();
-        $inserted_key = DB::connection('mysql2')->select(DB::raw("SELECT * from public_keys WHERE address = '".$request->input('address')."'"));
-        var_dump($inserted_key);
+        //$inserted_key = DB::connection('mysql2')->select(DB::raw("SELECT * from public_keys WHERE address = '".$request->input('address')."'"));
+        var_dump("SELECT * from public_keys WHERE address = '".$request->input('address')."'");
         die();
         if(empty($inserted_key)) {
             DB::connection('mysql2')->table('public_keys')->insert(['address' => $request->input('address'), 'public_key' => $request->input('public_key')]);
