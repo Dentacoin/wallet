5817fc5a14d78a08fe964b48e58704a7341f1064
unknown <miroslav.nedelchev@dentacoin.com>
2019-02-15 17:50:58 +0200

    test pub key creation

--- app/Http/Middleware/AdditionalMiddleware.php
+++ app/Http/Middleware/AdditionalMiddleware.php
@@ -16,10 +16,10 @@ class AdditionalMiddleware
      * @return mixed
      */
     public function handle($request, Closure $next) {
-        if(!isset($_COOKIE['testing-dev'])) {
+        /*if(!isset($_COOKIE['testing-dev'])) {
             return response(view('pages/maintenance'));
         }
-        $params = $request->route()->parameters();
+        $params = $request->route()->parameters();*/
         $response = (new App\Http\Controllers\Controller())->minifyHtml($next($request));
         return $response;
     }
