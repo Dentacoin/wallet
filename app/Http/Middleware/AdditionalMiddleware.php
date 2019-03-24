667201810fef9e567f71d1f78e19ffb17c22508d
unknown <miroslav.nedelchev@dentacoin.com>
2019-03-24 19:05:59 +0200

    push maintanance mod

--- app/Http/Middleware/AdditionalMiddleware.php
+++ app/Http/Middleware/AdditionalMiddleware.php
@@ -17,7 +17,7 @@ class AdditionalMiddleware
      */
     public function handle($request, Closure $next) {
         //if(!isset($_COOKIE['testing-dev'])) {
-        //return response(view('pages/maintenance'));
+        return response(view('pages/maintenance'));
 
         //$params = $request->route()->parameters();
         $response = (new App\Http\Controllers\Controller())->minifyHtml($next($request));
