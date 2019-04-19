b96e8f5cff791b1005381e0bdf641311c4c8fae2
unknown <miroslav.nedelchev@dentacoin.com>
2019-04-19 13:38:03 +0300

    test cross login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,6 +27,7 @@ class HomeController extends Controller     {
         ];
 
         var_dump(session('logged_user'));
+        var_dump(session('logged_user')['type']);
         if(empty(session('logged_user'))) {
             session(['logged_user' => $session_arr]);
             die('session created');
